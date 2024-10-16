b03add701dc4d0b694e17b1ed544a432c6c3c1ba
Laurent Hagimont <riff@pobox.com>
2024-10-16 17:09:43 +0200

    changed command line arguments

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
-	"strconv"
 	"strings"
 	"time"
 )
+
+const kDefaultArgDays = 7
 
 func main() {
 
@@ -16,36 +18,37 @@ func main() {
 		myOSNewLine = "\r\n"
 	}
 
-	if len(os.Args) <= 1 {
-		fmt.Print("Usage : osql-cleanup.exe <folder> [<days>]", myOSNewLine)
+	myPtrArgDir := flag.String("dir", "", "the folder to clean")
+	myPtrArgDays := flag.Int("days", 0, "the number of days to keep")
+    flag.Parse()
+
+	myArgsOK := true
+	if *myPtrArgDir == "" {
+		myArgsOK = false
+	}
+
+	myArgDays := *myPtrArgDays
+	if myArgDays == 0 {
+		myArgDays = kDefaultArgDays
+	}
+
+	if !myArgsOK {
+		fmt.Print("Usage : osql-cleanup.exe --dir=<folder> [--days=<days>]", myOSNewLine)
 		fmt.Print("<folder> is the path of the folder to scan, sub-folders are ignored", myOSNewLine)
-		fmt.Print("The .BAK files older than <days> days are deleted, default value is 7 days", myOSNewLine)
+		fmt.Print("The .BAK files older than <days> days are deleted, default value is ", kDefaultArgDays, " days", myOSNewLine)
 		fmt.Print("", myOSNewLine)
 		fmt.Print("The .BAK files can be created by the following command :", myOSNewLine)
 		fmt.Print("\"[PATH_TO]OSQL.exe\" -S <SQL_SERVER> -U <USER> -P <PASS> -Q \"BACKUP DATABASE <BASE_NAME> TO DISK='[EXPORT_PATH]%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%-<BASE_NAME>.BAK'\"", myOSNewLine)
 		return
 	}
 
-	myArgScanDirPath := os.Args[1]
+	myArgScanDirPath := *myPtrArgDir
 	myArgScanDirPath = strings.ReplaceAll(myArgScanDirPath, "\\\\", "\\")
 	myArgScanDirPath = strings.ReplaceAll(myArgScanDirPath, "\\", "/")
 	myArgScanDirPath = strings.TrimRight(myArgScanDirPath, "/")
 
-	myArgDaysAgo := 7
-	if len(os.Args) > 2 {
-		myArgDaysAgoStr := os.Args[2]
-		if myArgDaysAgoStr != "" {
-			myStrConvInt, myStrConvErr := strconv.Atoi(myArgDaysAgoStr)
-			if myStrConvErr != nil {
-				fmt.Print(myArgDaysAgoStr + " is not an integer : ", myStrConvErr, myOSNewLine)
-				return
-			}
-			myArgDaysAgo = myStrConvInt
-		}
-	}
-
 	fmt.Print("Scanning directory : ", myArgScanDirPath, myOSNewLine)
-	fmt.Print("Keep files for : ", myArgDaysAgo, " days", myOSNewLine)
+	fmt.Print("Keep files for : ", myArgDays, " days", myOSNewLine)
 
 	myScanFiles, myScanErr := os.ReadDir(myArgScanDirPath)
 	if myScanErr != nil {
@@ -72,7 +75,7 @@ func main() {
 		}
 						
 		myScanFileModTime := myScanFileInfo.ModTime()
-		myDaysAgo := time.Now().AddDate(0, 0, -myArgDaysAgo)
+		myDaysAgo := time.Now().AddDate(0, 0, -myArgDays)
 
 		if !myScanFileModTime.Before(myDaysAgo) {
 			continue
